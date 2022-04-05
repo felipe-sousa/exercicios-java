08a62e129bb986ca7c6f5e5c9515581bf6f27257
Felipe de Sousa <felipedesousa2001@gmail.com>
2022-04-05 20:21:47 -0300

    add exe14
    Adicionar Exercicio 14

--- src/Exe14.java
+++ src/Exe14.java
@@ -8,6 +8,23 @@ import java.util.Scanner;
 public class Exe14
 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int maior=0,menor=0,num=0;
 
+        //Solicita o primeiro número e atribui a variável Menor
+        System.out.print("Digite um número: ");
+        menor = input.nextInt();
+
+        // Solicita os outros 4 números e compara se são maiores ou menores que o Menor
+        for(int i=0; i<4; i++) {
+            System.out.print("Digite um número: ");
+            num = input.nextInt();
+
+            if(num > maior) maior = num;
+            if (num < menor) menor = num;
+        }
+
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
     }
 }
