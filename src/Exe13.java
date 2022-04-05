5c030ff6c6e003af835c9e9f968517d683205e8b
Felipe de Sousa <felipedesousa2001@gmail.com>
2022-04-05 19:11:45 -0300

    add exe13.java
    Adicionar o Exercício 13

--- src/Exe13.java
+++ src/Exe13.java
@@ -10,9 +10,29 @@ public class Exe13
 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num, qtdeNum;
+        int num1, num2, qtdNum, fat = 1; // Declaração das variáveis utilizadas
 
+        // Solicita o valor N
         System.out.print("Informe quantos valores devem ser digitados: ");
-        qtdeNum = input.nextInt();
+        qtdNum = input.nextInt();
+
+        // Solicita os valores enquanto N for maior que 0
+        while (qtdNum > 0) {
+
+            System.out.print("Informe o valor do número a ser fatorado: ");
+            num1 = num2 = input.nextInt();
+
+            // Faz o cálculo do fatorial
+            while (num2 > 0) {
+                fat *= num2--;
+            }
+
+            System.out.printf("O fatorial de %d é: %d ", num1, fat);
+            System.out.println();
+
+            // Diminui 1 do valor N e zera a variável do resultado da fatorial para calcular novamente
+            qtdNum--;
+            fat = 1;
+        }
     }
 }
